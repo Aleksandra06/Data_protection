821970130d64681e6f1a58c4ffb1e94e48d015d1
Aleksandra06 <plyaskina-sasha@mail.ru>
2020-09-03 14:52:38 +0700

    added the first task of the first lab

---
+++ DataProtection/DataProtection/PageModels/Lab1/Evklid.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataProtection.PageModels.Lab1
+{
+    public class Evklid
+    {
+        public long a { get; set; }
+        public long b { get; set; }
+        public long r { get; set; }
+    }
+}
---
+++ DataProtection/DataProtection/PageModels/Lab1/NumbersToPower.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataProtection.PageModels.Lab1
+{
+    public class NumbersToPower
+    {
+
+        public long a { get; set; }
+        public int x { get; set; }
+        public long y { get; set; }
+        public int p { get; set; }
+    }
+}
