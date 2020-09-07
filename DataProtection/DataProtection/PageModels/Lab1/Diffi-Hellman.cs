2b5a03b576b4519f4cb1396ee5f1794351ba045b
Aleksandra06 <plyaskina-sasha@mail.ru>
2020-09-07 19:15:19 +0700

    change TwoPage and ThreePage in lab 1

---
+++ DataProtection/DataProtection/PageModels/Lab1/Diffi-Hellman.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataProtection.PageModels.Lab1
+{
+    public class Diffi_Hellman
+    {
+        public long X { get; set; }
+        public long Y { get; set; }
+    }
+}
