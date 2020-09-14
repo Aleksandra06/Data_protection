701a6322450b24f3975f9bff93df7eaef7cc9ac6
SlavaYES <voen1999@gmail.com>
2020-09-14 22:51:25 +0700

    merge

---
+++ DataProtection/DataProtection/PageModels/Lab1/DisLog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+
+namespace DataProtection.PageModels.Lab1
+{
+    public class DisLog
+    {
+        public long a { get; set; }
+        public long y { get; set; }
+        public long x { get; set; }
+        public long p { get; set; }
+        public long m { get; set; }
+        public long k { get; set; } 
+    }
+}
