3ab7af92ecb2b9405dddeaae33b3fd44ff042372
Aleksandra06 <plyaskina-sasha@mail.ru>
2020-10-01 15:21:40 +0700

    added lab2 and upload file

---
+++ DataProtection/DataProtection/PageModels/DocumentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataProtection.PageModels
+{
+    public class DocumentModel
+    {
+        [MaxLength(50)]
+        public string FileName { get; set; }
+        [MaxLength(100)]
+        public string ContentType { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
