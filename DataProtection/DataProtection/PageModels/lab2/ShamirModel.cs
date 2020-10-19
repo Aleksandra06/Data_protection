9ef9571b34178ca6b4918d1d6d992711b00516be
SlavaYES <voen1999@gmail.com>
2020-10-19 22:57:17 +0700

    Change and finish generate

--- DataProtection/DataProtection/PageModels/lab2/ShamirModel.cs
+++ DataProtection/DataProtection/PageModels/lab2/ShamirModel.cs
@@ -7,11 +7,15 @@ namespace DataProtection.PageModels.lab2
 {
     public class ShamirModel
     {
-        public ulong p { get; set; } // 10 ^ 19
-        public ulong Ca { get; set; }
-        public ulong Da { get; set; }
-        public ulong Cb { get; set; }
-        public ulong Db { get; set; }
+        public long p { get; set; } // 10 ^ 19 / 2
+        public long Ca { get; set; }
+        public long Da { get; set; }
+        public long Cb { get; set; }
+        public long Db { get; set; }
+        public long x1 { get; set; }
+        public long x2 { get; set; }
+        public long x3 { get; set; }
+        public long x4 { get; set; }
 
     }
 }
