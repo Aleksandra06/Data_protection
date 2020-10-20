b371e77fd9193d3042f55667b86d408ef1162b52
Aleksandra06 <plyaskina-sasha@mail.ru>
2020-10-21 01:02:03 +0700

    change save file

---
+++ DataProtection/DataProtection/wwwroot/js/Script.js
@@ -0,0 +1,18 @@
+﻿function scrollToElementId(hash) {
+    const el = document.getElementById(hash);
+    el.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+        alignToTop: false
+    });
+
+}
+
+function saveAsFile(filename, bytesBase64) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:application/octet-stream;base64," + bytesBase64;
+    document.body.appendChild(link); // Needed for Firefox
+    link.click();
+    document.body.removeChild(link);
+}
