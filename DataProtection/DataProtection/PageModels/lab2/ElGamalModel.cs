514425f96fec12af1fbcbfa0ee8d929cdf3baf9f
SlavaYES <voen1999@gmail.com>
2020-10-20 23:52:43 +0700

    begin two

---
+++ DataProtection/DataProtection/PageModels/lab2/ElGamalModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataProtection.PageModels.lab2
+{
+    public struct Message
+    {
+        public long r { get; set; }
+        public long e { get; set; }
+    };
+    public class ElGamalModel
+    {
+        public long p { get; set; }
+        public long q { get; set; }
+        public long g { get; set; }
+        public long Da { get; set; }
+        public long Ca { get; set; }
+        public Message[] message { get; set; }
+
+    }
+}
