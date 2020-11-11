62a04165b809c34d872641e8d2ed98c54d177ffd
SlavaYES <voen1999@gmail.com>
2020-11-11 22:43:43 +0700

    add variable in model

--- DataProtection/DataProtection/PageModels/Lab3/ElGamalSignatureModel.cs
+++ DataProtection/DataProtection/PageModels/Lab3/ElGamalSignatureModel.cs
@@ -1,33 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace DataProtection.PageModels.Lab3
 {
     public class ElGamalSignatureModel
     {
+        public long p { get; set; }
+        public long q { get; set; }
+        public long x { get; set; }
+        public long y { get; set; }
+        public long g { get; set; }
+        public long r { get; set; }
+        public long k { get; set; }
+        public long u { get; set; }
+        public long s { get; set; }
+        public long p_prev { get; set; }
+        public long h { get; set; }
+
     }
 }
-
-//using System;
-//using System.Collections.Generic;
-//using System.Diagnostics;
-//using System.IO;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using BlazorInputFile;
-//using MatBlazor;
-
-//namespace DataProtection.Pages.Lab3
-//{
-//    public partical class ElGamalSignaturePage
-//    {
-//        protected override void OnInitialized()
-//        {
-
-//        }
-//    }
-//}
-
-//ElGamalSignaturePage.razor.cs
