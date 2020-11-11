a6052bdb3a387f509bbacacbe64f4539f8326ee3
SlavaYES <voen1999@gmail.com>
2020-11-11 19:23:10 +0700

    Begin lab3 Signature, El-Gamal

---
+++ DataProtection/DataProtection/PageModels/Lab3/ElGamalSignatureModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataProtection.PageModels.Lab3
+{
+    public class ElGamalSignatureModel
+    {
+    }
+}
+
+//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.IO;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using BlazorInputFile;
+//using MatBlazor;
+
+//namespace DataProtection.Pages.Lab3
+//{
+//    public partical class ElGamalSignaturePage
+//    {
+//        protected override void OnInitialized()
+//        {
+
+//        }
+//    }
+//}
+
+//ElGamalSignaturePage.razor.cs
---
+++ DataProtection/DataProtection/Pages/Lab3/ElGamalSignaturePage.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataProtection.Pages.Lab3
+{
+    public partial class ElGamalSignaturePage
+    {
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+    }
+}
