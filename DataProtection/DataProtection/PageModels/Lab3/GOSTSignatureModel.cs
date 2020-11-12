dd96a2c921b0e926032d8707315d2e85ff0cc19d
SlavaYES <voen1999@gmail.com>
2020-11-13 00:33:11 +0700

    begin initialized gost

---
+++ DataProtection/DataProtection/PageModels/Lab3/GOSTSignatureModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace DataProtection.PageModels.Lab3
+{
+    public class GOSTSignatureModel
+    {
+        public BigInteger p { get; set; }
+        public BigInteger q { get; set; }
+        public int b { get; set; }
+        public BigInteger a { get; set; }
+        public BigInteger x { get; set; }
+        public BigInteger y { get; set; }
+        public BigInteger h { get; set; }
+        public BigInteger h_revers { get; set; }
+        public BigInteger k { get; set; }
+        public BigInteger r { get; set; }
+        public BigInteger s { get; set; }
+        public BigInteger h_check { get; set; }
+        public BigInteger u1 { get; set; }
+        public BigInteger u2 { get; set; }
+        public BigInteger v { get; set; }
+
+    }
+}
---
+++ DataProtection/DataProtection/Pages/Lab3/GOSTSignaturePage.razor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataProtection.PageModels;
+using DataProtection.PageModels.Lab3;
+using DataProtection.Pages.Lab3;
+using Microsoft.AspNetCore.Components;
+
+namespace DataProtection.Pages.Lab3
+{
+    public partial class GOSTSignaturePage
+    {
+        [Parameter] public DocumentModel Document { get; set; }
+        public GOSTSignatureModel gost = new GOSTSignatureModel();
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+        public void generate()
+        {
+
+        }
+    }
+}
