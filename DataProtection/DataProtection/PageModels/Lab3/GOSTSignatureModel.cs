0c564046a90730cd056f8aedaecd06c74c031638
Aleksandra06 <plyaskina-sasha@mail.ru>
2020-11-14 03:28:27 +0700

    Gost edit

--- DataProtection/DataProtection/PageModels/Lab3/GOSTSignatureModel.cs
+++ DataProtection/DataProtection/PageModels/Lab3/GOSTSignatureModel.cs
@@ -1,28 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Threading.Tasks;
+﻿using Org.BouncyCastle.Math;
 
 namespace DataProtection.PageModels.Lab3
 {
     public class GOSTSignatureModel
     {
-        public System.Numerics.BigInteger p { get; set; }
-        public System.Numerics.BigInteger q { get; set; }
-        public int b { get; set; }
-        public System.Numerics.BigInteger a { get; set; }
-        public System.Numerics.BigInteger x { get; set; }
-        public System.Numerics.BigInteger y { get; set; }
-        public System.Numerics.BigInteger h { get; set; }
-        public System.Numerics.BigInteger h_revers { get; set; }
-        public System.Numerics.BigInteger k { get; set; }
-        public System.Numerics.BigInteger r { get; set; }
-        public System.Numerics.BigInteger s { get; set; }
-        public System.Numerics.BigInteger h_check { get; set; }
-        public System.Numerics.BigInteger u1 { get; set; }
-        public System.Numerics.BigInteger u2 { get; set; }
-        public System.Numerics.BigInteger v { get; set; }
+        public BigInteger p { get; set; }
+        public BigInteger q { get; set; }
+        public BigInteger b { get; set; }
+        public BigInteger a { get; set; }
+        public BigInteger x { get; set; }
+        public BigInteger y { get; set; }
+        public BigInteger h { get; set; }
+        public BigInteger h_revers { get; set; }
+        public BigInteger k { get; set; }
+        public BigInteger r { get; set; }
+        public BigInteger s { get; set; }
+        public BigInteger h_check { get; set; }
+        public BigInteger u1 { get; set; }
+        public BigInteger u2 { get; set; }
+        public BigInteger v { get; set; }
 
     }
 }
--- DataProtection/DataProtection/Pages/Lab3/GOSTSignaturePage.razor.cs
+++ DataProtection/DataProtection/Pages/Lab3/GOSTSignaturePage.razor.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using DataProtection.PageModels;
 using DataProtection.PageModels.Lab3;
 using DataProtection.Pages.Lab3;
 using Microsoft.AspNetCore.Components;
+using Org.BouncyCastle.Math;
 
 namespace DataProtection.Pages.Lab3
 {
@@ -15,7 +16,7 @@ namespace DataProtection.Pages.Lab3
         [Parameter] public DocumentModel Document { get; set; }
         public GOSTSignatureModel gost = new GOSTSignatureModel();
         Random random = new Random();
-
+        //private const int b = Math 767;
 
         protected override void OnInitialized()
         {
@@ -23,17 +24,25 @@ namespace DataProtection.Pages.Lab3
         }
         public void generate()
         {
-            gost.p = generateP();
-        }
+            SHA256 sha256 = SHA256.Create();
+            // gost.h = new BigInteger(sha256.ComputeHash(Document.Data));
+            gost.p = BigInteger.ProbablePrime(1024, random);
+            BigInteger tmp = gost.p.Subtract(BigInteger.One);
+            BigInteger t;
+            do
+            {
+                gost.q = BigInteger.ProbablePrime(256, random);
+                t = tmp.Mod(gost.q);
 
-        private System.Numerics.BigInteger generateP()
-        {
-            System.Numerics.BigInteger currentQ;
-            //int b = random.Next(700, 800);
-            //do {
-                
-            //} while ();
-            throw new NotImplementedException();
+            } while (t.CompareTo(BigInteger.Zero) != 0);
+            gost.b = tmp.Divide(gost.q);
+
+            //a
+            //var stepen = new BigInteger(b.ToString()).Mod(gost.q);
+            //do
+            //{
+            //    gost.a = new BigInteger(random.Next().ToString());
+            //} while (gost.a.ModPow(gost.q, gost.p).CompareTo(BigInteger.One) != 0);
         }
     }
 }
