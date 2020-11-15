adc10fa91dfe08508079cdc76cca041b3f400f4b
Aleksandra06 <plyaskina-sasha@mail.ru>
2020-11-15 18:26:51 +0700

    add lab4

---
+++ DataProtection/DataProtection/Pages/Lab4/Poker.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace DataProtection.Pages.Lab4
+{
+    public class PokerViewModel : ComponentBase
+    {
+
+    }
+}
