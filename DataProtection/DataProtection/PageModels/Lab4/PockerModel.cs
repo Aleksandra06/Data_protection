1a311cd7b89ae5b3994999e72f550d75d056a108
Aleksandra06 <plyaskina-sasha@mail.ru>
2020-11-24 13:46:29 +0700

    change lab4

---
+++ DataProtection/DataProtection/PageModels/Lab4/PockerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataProtection.PageModels.Lab4
+{
+    public class PockerModel
+    {
+        [Range(2, 25, ErrorMessage = "Слишком мало или слишком много игроков")]
+        public int CountPeople { get; set; }
+
+
+    }
+}
--- DataProtection/DataProtection/Pages/Lab4/Poker.razor.cs
+++ DataProtection/DataProtection/Pages/Lab4/Poker.razor.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataProtection.PageModels.Lab4;
 using Microsoft.AspNetCore.Components;
 
 namespace DataProtection.Pages.Lab4
 {
     public class PokerViewModel : ComponentBase
     {
+        protected PockerModel Model { get; set; } = new PockerModel();
 
     }
 }
