0dbc0338ecf1e87c5a5183a0b19133f048e2138c
Aleksandra06 <plyaskina-sasha@mail.ru>
2020-12-09 01:11:22 +0700

    add rgz2

---
+++ DataProtection/DataProtection/Pages/RgzTwo/Edge.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataProtection.Pages.RgzTwo
+{
+    public class Edge
+    {
+        public int vertex1 { get; set; }
+        public int vertex2 { get; set; }
+        public int color1 { get; set; }
+        public int color2 { get; set; }
+    }
+}
